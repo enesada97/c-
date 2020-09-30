29e6cb390adc30fb66c2decb6e65de02340caac6
Enes Tuğrul ADA <eada997@gmail.com>
2020-09-30 14:25:27 +0300

    Add files via upload

---
+++ pdbFilesReader/WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button1.BackgroundImage")));
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button1.Location = new System.Drawing.Point(688, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(82, 41);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(356, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(333, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Lütfen PDB dosyanızı seçiniz :";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(379, 222);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(409, 121);
+            this.listBox1.TabIndex = 2;
+            this.listBox1.Tag = "";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(379, 138);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(409, 20);
+            this.textBox1.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(357, 67);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(410, 16);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Not: Dosyayı seçtiğinizde otomatik olarak veriler listelenir.";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Silver;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(800, 355);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ pdbFilesReader/WindowsFormsApp2/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+       
+        public Form1()
+        {
+            InitializeComponent();
+            button1.Click += new System.EventHandler(HandeAras);//butona tıklandığında bu metoda git.
+            
+        }
+        public void HandeAras(object sender,EventArgs e)
+        {
+            MessageBox.Show("Verileriniz Derlendi.");
+            button1.Enabled = false;//butonu etkisiz kıl
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Filter = "PDB Dosyası(*.pdb) |*.pdb";//pdb dosyası ile gözat ı filtrele
+            if(openFileDialog1.ShowDialog() == DialogResult.OK) //eğer dosya seçildiyse
+            {
+                FileInfo eskişehir = new FileInfo(openFileDialog1.FileName.ToString());//dosya bilgisini al 
+                StreamReader oku = eskişehir.OpenText();//dosyayı oku
+                string satır = oku.ReadLine();//dosyayı satır satır oku ve satıra eşitle
+                string ara = "ATOM";//atom ile başlayan metin kısmını bulmak için değişken
+
+                while (satır != null)//satır doluysa gir
+                {
+
+                    if(satır.StartsWith(ara))//eğer ara yani ATOM  ile başlarsa gir
+                    {
+                        string gecici = satır.Replace(ara, "");//string döndür
+                        string[] yapraksarma = gecici.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);//şu boşlukta şu kadar böl ve diziye at
+
+                            string numara = yapraksarma[0].ToString();//Atom numaralarının dizideki yeri
+                            string isim = yapraksarma[1].ToString();
+                            string koor = yapraksarma[5].ToString();
+                            
+                            listBox1.Items.Add("Atom Numarası=" + yapraksarma[0] + "   " + "Proteinin İsmi= " + yapraksarma[1] + "  " + "X koordinatı" +  yapraksarma[5] + "/n" );
+                            textBox1.Text = "En Büyük X koordinatına sahip atom = " + yapraksarma[5].Max() + "  " + "Atom Adı= " + yapraksarma[1] + "  " + "Numarası= " + yapraksarma[0] ;
+                        
+
+                    }
+                    satır = oku.ReadLine();//döngü için tekrar oku
+                }
+                
+                
+                oku.Close();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
